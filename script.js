757284da8b3be3151fdac8d5ccf9976170495b85
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-03-18 21:10:46 +0900

    Update playround function to be more efficient

--- script.js
+++ script.js
@@ -2,19 +2,22 @@ const playerSelectionDisplay = document.getElementById('player-selection');
 const computerSelectionDisplay = document.getElementById('computer-selection');
 const roundResultDisplay = document.getElementById('round-result');
 const possibleSelections = document.querySelectorAll('button');
-const playerScoreDisplay = document.getElementById('player-score'); // ADDED
-const computerScoreDisplay = document.getElementById('computer-score'); // ADDED
-const gameResultDisplay = document.getElementById('game-result'); // ADDED
+const playerScoreDisplay = document.getElementById('player-score'); 
+const computerScoreDisplay = document.getElementById('computer-score'); 
+const gameResultDisplay = document.getElementById('game-result'); 
+const roundNumberDisplay = document.getElementById('round-number'); 
 let playerSelection;
 let computerSelection;
 let roundResult;
-let playerScore; // ADDED
-let computerScore; // ADDED
+let playerScore = 0; // ADDED
+let computerScore = 0; // ADDED
+let roundNumber = 1; // ADDED
 let gameResult; // ADDED
 
 possibleSelections.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
     playerSelection = e.target.id;
     //console.log(playerSelection); // TEST
+    //return playerSelection; //ADDED
     playerSelectionDisplay.innerHTML = playerSelection;
     getComputerSelection();
     playRound();
@@ -27,100 +30,79 @@ function getComputerSelection() {
     //console.log(randomNumber) //TEST
 
     if (randomNumber == 0) {
-        computerSelection = "rock";
+        computerSelection = "rock"; 
     } else if (randomNumber == 1) {
         computerSelection =  "paper";
     } else if (randomNumber == 2) {
         computerSelection =  "scissors";
     } else {
-        computerSelection = "Oops. Something went wrong";
+        computerSelection = "Oops. Something went wrong"; 
     }
 
     //console.log(computerSelection); // TEST
     computerSelectionDisplay.innerHTML = computerSelection;
+    return computerSelection;
   }
 
-  /*
 // Function that will play a single round of ‘Rock’, ‘Paper’ or ‘Scissors’. 
-function playRound() {
+function playRound(playerSelection, computerSelection) {
+    //playerSelection = gameResultDisplay();
+    computerSelection = getComputerSelection();
 
     if (playerSelection == computerSelection) {
-        roundResult = "It's a DRAW this round";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundResult =  "Computer wins. You LOSE this round";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundResult = "Player wins. You WIN this round";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundResult = "Player wins. You WIN this round";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundResult = "Computer wins. You LOSE this round";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundResult = "Computer wins. You LOSE this round";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        roundResult = "Player wins. You WIN this round";
-    } else {
-        roundResult = "Oops. Something went wrong";
-    }
-
+        roundResult = "It's a DRAW!!! Play this round again."; 
+    } else if 
+        ((playerSelection == "rock" && computerSelection == "paper") || 
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissors" && computerSelection == "rock")) {
+        roundResult =  "Computer wins. You LOSE this round."; 
+        computerScore += 1; 
+        roundNumber += 1;
+    } else if 
+        ((playerSelection == "rock" && computerSelection == "scissors") ||
+        (playerSelection == "paper" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "paper")) {
+        roundResult = "Player wins. You WIN this round.";
+        playerScore += 1; 
+        roundNumber += 1;
+        }
     //console.log(roundResult); // TEST
     roundResultDisplay.innerHTML = roundResult;
-}
-*/
-// UPDATED FUNCTION ---COMPARE TO PREVIOUS
-function playRound() {
-
-    if (playerSelection == computerSelection) {
-        roundResult = "It's a DRAW this round";
-    } else if (playerSelection == "rock" && computerSelection == "paper") {
-        roundResult =  "Computer wins. You LOSE this round";
-    } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        roundResult = "Player wins. You WIN this round";
-    } else if (playerSelection == "paper" && computerSelection == "rock") {
-        roundResult = "Player wins. You WIN this round";
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        roundResult = "Computer wins. You LOSE this round";
-    } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        roundResult = "Computer wins. You LOSE this round";
-    } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        roundResult = "Player wins. You WIN this round";
-    } else {
-        roundResult = "Oops. Something went wrong";
-    }
-
-    //console.log(roundResult); // TEST
-    roundResultDisplay.innerHTML = roundResult;
-
+    playerScoreDisplay.innerHTML = playerScore; // Update the player score display
+    computerScoreDisplay.innerHTML = computerScore; // Update the computer score display
+    roundNumberDisplay.innerHTML = roundNumber; // Update the computer score display
     return roundResult;
 }
 
 // TO FIX!-------------------------------------------------------------------------------
 // Function that will play five rounds of ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function game() {
-    playerScore = 0;
-    computerScore = 0;
   
     for (let i = 0; i < 5; i++) {
-      playRound(); // Use the global playerSelection and computerSelection variables
-      if (roundResult == "Player wins. You WIN this round") {
-        playerScore += 1;
-      } else if (roundResult == "Computer wins. You LOSE this round") {
-        computerScore += 1;
-      }
+      playRound(playerSelection, computerSelection); // Use the global playerSelection and computerSelection variables
+        if (roundResult == "Player wins. You WIN this round") {
+        playerScore += 1; 
+        } else if (roundResult == "Computer wins. You LOSE this round") {
+        computerScore += 1; 
+        }
     }
   
     if (playerScore == 5) {
-      gameResult == "PLAYER WINS THE GAME!";
+      gameResult == "PLAYER WINS THE GAME!"; 
     } else if (computerScore == 5) {
-      gameResult = "COMPUTER WINS THE GAME.... better luck next time.";
+      gameResult = "COMPUTER WINS THE GAME.... better luck next time."; 
+    } else if (playerScore < 5 && computerScore < 5) {
+        gameResult = ".... who will win the game?....."
+    } else {
+        gameResult = "Oops. Something went wrong"; 
     }
 
     gameResultDisplay.innerHTML = gameResult;
-    playerScoreDisplay.innerHTML = playerScore; // Update the player score display
-    computerScoreDisplay.innerHTML = computerScore; // Update the computer score display
 
-    return gameResult; 
 }
-  
+
+
+
   
 
 

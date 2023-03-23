83278d40a6cf842fd313f5c794bddcd6c0f912b9
Guy Hennequin <guy.a.hennequin@gmail.com>
2023-03-23 21:38:22 +0900

    Update JS file with winner array and game function

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 //This game will be played from the console for now
 
 const selections = ["rock", "paper", "scissors"] // possible choices in an array
+const roundWinnerList = []; // array to keep a list of round winners
 
 // Function that will return the users selection of ‘Rock’, ‘Paper’ or ‘Scissors’.
 function getPlayerChoice() { 
-  let playerInput = prompt("Player/'s turn! Make your selection by typing: Rock, Paper, or Scissors"); //prompt player to type
+  let playerInput = prompt("Player's turn! Make your selection by typing: Rock, Paper, or Scissors"); //prompt player to type
   while (playerInput == null) { //to execute when user clicks cancel
     playerInput = prompt("This pop up cannot be cancelled. Please make your selection by typing: Rock, Paper, or Scissors");
   }
   playerInput = playerInput.toLowerCase(); //changes users input to lowercase
-  let check = validatePlayerInput(playerInput);
-  while (check == false) {
-    prompt("Please check the spelling of: Rock, Paper, or Scissors");
+  let check = validatePlayerInput(playerInput); // calls function to validate user's input
+  while (check == false) { //to execute when user leaves blank or misspells
+    playerInput = prompt("Please check the spelling of: Rock, Paper, or Scissors");
+    while (playerInput == null) { //to exit the above loop ------ TO FIX
+      playerInput = prompt("Please make your selection by typing: Rock, Paper, or Scissors"); 
+    }
+    playerInput = playerInput.toLowerCase;
+    check = validatePlayerInput(playerInput);
   }
-  //console.log(playerInput);
+  return playerInput;
+  //console.log(playerInput); //TEST
 } 
+
 
 // Function that will validate that the user typed ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function validatePlayerInput(playerInput) {
-  if (selections.includes(playerInput)) {
+  if (selections.includes(playerInput)) { // matching selections variable and playerInput
     return true;
   } else {
     return false;
   }
 }
+
 
 // Function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function getComputerChoice() {
@@ -32,7 +41,7 @@ function getComputerChoice() {
   /* INITIAL THOUGHT 
    const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber == 0) {
-        return computerSelectiplayRoundon = "rock"; 
+        return computerSelection = "rock"; 
     } else if (randomNumber == 1) {
         return computerSelection =  "paper";
     } else {
@@ -40,16 +49,47 @@ function getComputerChoice() {
     }*/
 }
 
+
 // Function that will play a single round of ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function playRound() {
   const playerSelection = getPlayerChoice(); //uses return from getPlayerChoice function
+  console.log(playerSelection); //TEST
   const computerSelection = getComputerChoice(); //uses return from getComputerChoice function
-
+  console.log(computerSelection); //TEST
+  const roundWinner = getRoundWinner(playerSelection, computerSelection); //uses return from getRoundWinner function
+  console.log(roundWinner); //TEST
+  roundWinnerList.push(roundWinner); //add roundWinner to the roundWinnerList array
 }
 
-// Function that will play a game of ‘Rock’, ‘Paper’ or ‘Scissors’ where the first to 5 wins. 
+
+// Function that will decide the winner of a round
+function getRoundWinner(playerChoice, computerChoice) {
+  if (playerChoice == computerChoice) {
+    return "It's a DRAW!!! Play this round again."; 
+  } else if (
+    (playerChoice == "rock" && computerChoice == "paper") || 
+    (playerChoice == "paper" && computerChoice == "rock") ||
+    (playerChoice == "scissors" && computerChoice == "rock")
+    ) {
+      return "Computer wins. You LOSE this round."; 
+    } else {
+      return "Player wins. You WIN this round.";
+    }
+  }
+
+
+// Function that will keep a log of the winners of each round
+function countWins() {
+  console.log(roundWinnerList); //OPTIMISE WITH A FILTER
+}
+
+
+// Function that will play a game of ‘Rock’, ‘Paper’ or ‘Scissors’ where the first to 5 wins 
 function game() {
-  playRound(); //calls playRound function
+  for (let i = 1; i < 6; i++) { // start at round 1, plays 5 rounds, add one round each loop
+    playRound(); //uses playRound function return
+  }
+  countWins(); //uses countWins function return
 }
 
 game();
